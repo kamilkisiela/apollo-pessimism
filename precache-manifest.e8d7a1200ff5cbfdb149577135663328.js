07e5dbc027c136fdeb1f38b9d286e431a73cd8eb
kamilkisiela <kamilkisiela@users.noreply.github.com>
2020-09-28 14:58:07 +0000

    deploy: 587fb0d6a874e6d526d486414403a0c4c00a88fa

---
+++ precache-manifest.e8d7a1200ff5cbfdb149577135663328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7944b47ccb7d83274675ea4f0f8a7006",
+    "url": "/index.html"
+  },
+  {
+    "revision": "602c79ca91bfabd172ac",
+    "url": "/static/js/2.f8f18cb1.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.f8f18cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126b76f33484e52513ad",
+    "url": "/static/js/main.ed568fcb.chunk.js"
+  },
+  {
+    "revision": "ad41ed7970bde6259ad7",
+    "url": "/static/js/runtime-main.836142ac.js"
+  }
+]);
