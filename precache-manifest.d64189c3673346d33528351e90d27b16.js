708825d4984b57ca74cfeca964f17c98b649b6c2
kamilkisiela <kamilkisiela@users.noreply.github.com>
2020-09-28 15:01:29 +0000

    deploy: 7d4e728dcbb6413746180dc78c8810effe2c20f5

---
+++ precache-manifest.d64189c3673346d33528351e90d27b16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9187a5de5b8711069c2e98c4f1cf3495",
+    "url": "/apollo-pessimism/index.html"
+  },
+  {
+    "revision": "602c79ca91bfabd172ac",
+    "url": "/apollo-pessimism/static/js/2.f8f18cb1.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/apollo-pessimism/static/js/2.f8f18cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126b76f33484e52513ad",
+    "url": "/apollo-pessimism/static/js/main.ed568fcb.chunk.js"
+  },
+  {
+    "revision": "a9fe8c9c4d19133f2c5c",
+    "url": "/apollo-pessimism/static/js/runtime-main.c59c2e31.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8d7a1200ff5cbfdb149577135663328.js"
+  "/apollo-pessimism/precache-manifest.d64189c3673346d33528351e90d27b16.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/apollo-pessimism/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
